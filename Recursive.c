17b5db295bb4727b8319c6e65b91431fe8933906
veve-droid <78753781+veve-droid@users.noreply.github.com>
2021-02-21 12:15:35 +0800

    Update Recursive.c

--- Recursive.c
+++ Recursive.c
@@ -1,102 +1,200 @@
-#include<stdio.h>
 
-int isPerfNum(int x);
-int power(int x, int y);
-int factorial(int n);
-int fibonacci(int n);
-int summation(int n,int i);
-int base10to8(int n,int i,int j);
+#include <stdio.h>
 
-int main() {
-	int alive=1,option=0,invalid=1,input1,input2;
-	while(alive){
-		printf("===============MENU===============\n== (1) Perfect Number or Not\t==\n== (2) X Raised to Y\t\t==\n== (3) Factorial\t\t==\n== (4) Fibonacci\t\t==\n== (5) Summation\t\t==\n== (6) Decimal to Octal\t\t==\n== (7) Exit\t\t\t==\n===============MENU===============");
-		printf("\nChoose an option: ");
-		scanf("%d",&option);
-		switch(option){
-			case 1:
-				while(invalid){
-					printf("\n(1) Perfect Number or Not\nEnter a positive number:");
-					scanf("%d",&input1);
-					if(input1>0){
-						if(isPerfNum(input1)){printf("%i is a Perfect Number",input1);}else printf("%i is not a Perfect Number", input1);
-						break;
-					}
-					printf("\nNot a valid input!\n");
-				} 
-				break;
-			case 2:
-				printf("\n(2) X Raised to Y\nEnter two integers, x and y: ");
-				scanf("%i %i",&input1,&input2);
-				if(input2<0){printf("\n%i raised to %i is equal to 1/%i\n",input1,input2,power(input1,input2));}else printf("\n%i raised to %i is equal to %i\n",input1,input2,power(input1,input2));
-				break;
-			case 3:
-				while(invalid){
-					printf("(3) Factorial\nEnter a non-negative number: ");
-					scanf("%i",&input1);
-					if(input1>=0){
-						printf("\n%i! is equal to %i",input1,factorial(input1));
-						break;
-					}
-					printf("\nNot a valid input!\n");
-				}
-				break;
-			case 4:
-				while(invalid){
-					printf("(4) Fibonacci\nEnter a positive number: ");
-					scanf("%d",&input1);
-					if(input1>0){
-						printf("\nThe %i",input1);
-						if(input1%10==1){printf("st");}else if(input1%10==2){printf("nd");}else if(input1%10==3){printf("rd");}else printf("th");; 
-						printf(" element in the Fibonnaci Sequence is %i",fibonacci(input1));
-						break;
-					}
-					printf("\nNot a valid input!\n");
-				}
-				break;
-			case 5:
-				while(invalid){
-					printf("(5) Summation\nEnter a positive number: ");
-					scanf("%d",&input1);
-					if(input1>0){
-						printf("\nThe sum of all numbers from 1 to %d is %d",input1,summation(input1,0));
-						break;
-					}
-					printf("\nNot a valid input!\n");
-				}
-				break;
-			case 6:
-				while(invalid){
-					printf("(6) Decimal to Octal\nEnter a positive number: ");
-					scanf("%d",&input1);
-					if(input1>0){
-						printf("\n\n%d",base10to8(input1,0,0));
-						break;
-					}
-					printf("\nNot a valid input!\n");
-				}
-				break;
-			case 7:
-				printf("\n\nTERMINATED PROGRAM");
-				return 0;
-			default:
-				printf("\nOption does not exist!");
-		}
-		printf("\n\nEnter \"1\" to continue, all other inputs Exit: ");
-		scanf("%d",&option);
-		if(!(option==1)){printf("\n\nTERMINATED PROGRAM");return 0;}
-		printf("\n\n\n");
-		option = 0;
+int perfectNumber(int);
+double expo(int x, int y);
+long long int factorial(int);
+long int fibonacci(int n);
+int sum(int);
+void decToOctal(int);
+
+
+int main(){
+int x, y, valid = 1, a, b , temp = b;
+int num; 
+	
+	do{
+	
+		printf("\n========= MENU =========\n\n");
+		printf("1.Perfect Number or Not\n");
+		printf("2.X raised to Y\n");
+		printf("3.Factorial\n");
+		printf("4.Fibonacci\n");
+		printf("5.Summation\n");
+		printf("6.Decimal to Octal\n");
+		printf("7.Exit\n\n");
+		printf("Choose from the menu: ");
+		scanf("%d", &num);
+			
+	}while(num<1);
+	
+	
+	if(num > 6)
+		return 0; ///* exit *///
 		
+	
+	if(num == 1 || num == 4 || num == 5 || num == 6)
+	 	y = 1; //* positive number *//
+	if(num == 3)
+		y = 0; //* non-negative integer *//
+		
+	
+	
+	
+	
+	if(num == 2){
+	
+		do{
+		
+			printf("Enter two integers: ");
+			scanf("%i%i", &a, &b);
+			
+			temp = b;
+			valid = 1;
+			
+			if(a == 0 && b == 0){
+				printf("Undefined! Try other values\n\n");
+				valid = 0;
+			}
+			
+			if(b < 0)
+				b = b * -1;
+			
+		}while(!valid);
+		
+		
+		if(temp<0)
+			printf("%d raised to -%d is %.5lf", a, b, 1/expo(a,-1 * b));
+		else 
+			printf("%d raised to %d is %.0lf", a, b, expo(a,b));
 	}
-
+	
+		
+	else {
+	
+		do{
+		
+			printf("Enter a positive number: ");
+			scanf("%i", &x);
+			
+		}while(x<y);
+	
+	if(num == 1){
+		if(perfectNumber(x))
+			printf("%i is a perfect number.\n", x);
+		else 
+			printf("%i is not a perfect number.\n", x);
+	}
+	
+	
+	
+	if(num == 3)
+		printf("%i! = %lld\n", x, factorial(x));
+	else if(num == 4)
+		printf("%i element in the fibonacci sequence is %ld.\n", x, fibonacci(x));	
+	else if(num == 5)
+		printf("The summation from 1 to %i is %i\n", x, sum(x));
+	else if(num == 6)
+		decToOctal(x);
+	
+	
+	}
+	
+	
+	
+		
+	printf("\n\n");		
 	return 0;
+	
 }
 
-int isPerfNum(int x){int n=0,i;for(i=1;i<=x/2;i++)if(x%i==0)n+=i;if(n==x){ return 1;}else return 0;}
-int power(int x,int y){int n=x,i;if(y!=0){if(y<0)y*=-1;for(i=1;i<y;i++)n*=x;}else n=1;return n;}
-int factorial(int n){int x=1,i;for(i=n;i>1;i--)x*=i;return x;}
-int fibonacci(int n){int x=0,y=1,i;for(i=1;i<n;i++){x+=y;y=x-y;}return x;}
-int summation(int n,int i){if(n>0){return summation(n-1,i+n);}else return i;}
-int base10to8(int n,int i,int j){i +=((n%8)*power(10,j));if(n>8){i=base10to8((n/8),i,++j);}return i;}
 
+
+
+///////////////// functions /////////////////
+
+
+
+	int perfectNumber(int n){
+
+		int i, sum=0;
+				
+			for(i=2; i * i < n; i++){
+			
+				if(n % i == 0)
+					sum = sum+i+(n/i);
+					
+			}
+			
+			if(n != 1 && sum + 1 == n)
+				return 1;
+			else 
+				return 0;
+
+	}
+
+
+
+	double expo(int x, int y){
+		
+		if(y == 0)
+			return 1;
+		
+		if(y == 1 || y == -1)
+			return x;
+					
+		else{
+			if(y % 2 == 0)
+				return expo(x * x, y/2);
+			else
+				return x * expo(x * x, y/2);
+		}
+		
+	}
+	
+	
+
+	long long int factorial(int n){
+		
+		if(n == 0 || n == 1)
+			return 1;
+		else
+			return n * factorial(n-1);
+
+	}
+
+
+
+	long int fibonacci(int n){
+
+		if(n == 1 || n == 2)
+			return 1;
+		else
+			return fibonacci(n-2) + fibonacci(n-1);
+
+	}
+
+
+
+	int sum(int n){
+		
+		if(n==1)
+			return 1;
+		else
+			return n + sum(n-1);
+
+	}
+
+
+
+	void decToOctal(int n){
+
+		if(n>0){
+		
+			decToOctal(n/8);
+			printf("%i", n % 8);
+			
+		}
+		
+	} 
+	
